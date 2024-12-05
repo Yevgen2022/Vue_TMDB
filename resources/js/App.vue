d3425eb87b5c15cca565ec074ac916d47539ef29
Yevhen <yoshkukov@Yevhens-MBP.localdomain>
2024-12-05 15:53:32 +0100

    Initial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <!-- Your component's HTML goes here -->
+        <ExampleComponent />
+    </div>
+</template>
+
+<script setup>
+
+import ExampleComponent from "./components/ExampleComponent.vue";
+
+</script>
+
+<style scoped>
+/* component styles go here */
+</style>
